a5a21d2757dc5a7c45c1262b7083da6eccbfa059
SofiaBManzano <sofiabmanzano@gmail.com>
2022-03-07 20:29:58 +0100

    new smile

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,11 +1,12 @@
 import Description from "./Description";
 import Hello from "./Hello";
-
+import Smile from "./Smile";
 // import "../styles/WhoIAm.scss";
 const WhoIAm = (props) => {
   return (
     <div className={`container ${props.backgroundImageHello}`}>
       <Hello borderText={props.borderText} />
+      <Smile />
       <Description paragraphColor={props.paragraphColor} />
     </div>
   );
---
+++ src/components/Landing/Smile.js
@@ -0,0 +1,9 @@
+const Smile = () => {
+  return (
+    <article className="smile">
+      {/* // <p>// do you prefer to see the full color version?</p> */}
+      <span className="smile__face">:)</span>
+    </article>
+  );
+};
+export default Smile;
